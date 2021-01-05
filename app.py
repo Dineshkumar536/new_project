4f56a662d5648007d2559d97ffb5174cba145ecf
Dinesh kumar <dineshrajput828282@gmail.com>
2021-01-05 23:17:57 +0530

    updated

--- app.py
+++ app.py
@@ -27,23 +27,59 @@ def sentiment_nlp():
 def sentiment():
     if request.method == 'POST':
         message = request.form['message']
-        text = [message]
-        data = preprocessing.text_Preprocessing(text)
-        vect = cv.transform(text)
-        my_prediction = clf.predict(vect)
-        Keywords = keywords(message)
-        Keywords = 'Keywords =' + str(Keywords)
-        new = Keywords.split('\n')
         newstr = ''
-        for word in new:
-            newstr = newstr + word + ', '
-        sum_message = summarize(message)
-        overview = TextBlob(message)
-        Polarity = round(overview.sentiment.polarity,2)
-        Polarity = 'Polarity =' + str(Polarity)
-        Subjectivity = round(overview.sentiment.subjectivity,2)
-        Subjectivity = 'Subjectivity =' + str(Subjectivity)
-        return render_template('sentiment.html', prediction=my_prediction,newstr=newstr,sum_message=sum_message,Polarity=Polarity,Subjectivity=Subjectivity)
+        sum_message = ''
+        alert = ''
+        my_prediction = ''
+        Polarity = ''
+        Subjectivity = ''
+        if len(message)<2:
+            alert='enter text more than 2 character'
+            print(alert)
+        elif len(message)>2 and len(message)<100:
+            text = [message]
+            data = preprocessing.text_Preprocessing(text)
+            vect = cv.transform(text)
+            my_prediction = clf.predict(vect)
+            overview = TextBlob(message)
+            Polarity = round(overview.sentiment.polarity,2)
+            Polarity = 'Polarity =' + str(Polarity)
+            Subjectivity = round(overview.sentiment.subjectivity,2)
+            Subjectivity = 'Subjectivity =' + str(Subjectivity)
+        elif len(message)>100 and len(message)<250:
+            text = [message]
+            data = preprocessing.text_Preprocessing(text)
+            vect = cv.transform(text)
+            my_prediction = clf.predict(vect)
+            Keywords = keywords(message)
+            Keywords = 'Keywords =' + str(Keywords)
+            new = Keywords.split('\n')
+            newstr = ''
+            for word in new:
+                newstr = newstr + word + ', '
+            overview = TextBlob(message)
+            Polarity = round(overview.sentiment.polarity,2)
+            Polarity = 'Polarity =' + str(Polarity)
+            Subjectivity = round(overview.sentiment.subjectivity,2)
+            Subjectivity = 'Subjectivity =' + str(Subjectivity)
+        elif len(message)>250:
+            text = [message]
+            data = preprocessing.text_Preprocessing(text)
+            vect = cv.transform(text)
+            my_prediction = clf.predict(vect)
+            Keywords = keywords(message)
+            Keywords = 'Keywords =' + str(Keywords)
+            new = Keywords.split('\n')
+            newstr = ''
+            for word in new:
+                newstr = newstr + word + ', '
+            sum_message = summarize(message)
+            overview = TextBlob(message)
+            Polarity = round(overview.sentiment.polarity,2)
+            Polarity = 'Polarity =' + str(Polarity)
+            Subjectivity = round(overview.sentiment.subjectivity,2)
+            Subjectivity = 'Subjectivity =' + str(Subjectivity)
+    return render_template('sentiment.html', prediction=my_prediction,newstr=newstr,sum_message=sum_message,Polarity=Polarity,Subjectivity=Subjectivity,alert=alert)
 
 
 if __name__ == '__main__':
